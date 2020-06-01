574cd03b6e2965eb35e101effc1eca8c5370e0f1
Deivid Roger Oliveira Santos <deivid@deividroger.net>
2020-05-31 23:18:47 -0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MinhaDemoMVC.Models;
+
+namespace MinhaDemoMVC.Controllers
+{
+    [Route("")]
+    [Route("gestao-clientes")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id:int}/{categoria:guid}/{categorias?}")]
+        public IActionResult Index(string id, Guid categoria, string categorias)
+        {
+            return View();
+        }
+
+        [Route("sobre")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Route("contato")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [Route("privacidade")]
+        [Route("politica-de-privacidade")]
+        public IActionResult Privacy(Filme filme)
+        {
+            if (!ModelState.IsValid) {
+                foreach (var item in ModelState.Values.SelectMany(m => m.Errors))
+                {
+                    Console.WriteLine(item.ErrorMessage);
+                }
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MinhaDemoMVC.Models
+{
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="O campo titulo é obrigatório")]
+        [StringLength(60,MinimumLength = 3,ErrorMessage ="O tituto precisa ter entre 3 ou 60 caracteres")]
+        [Display(Name ="Titulo")]
+        public string Titulo { get; set; }
+
+        [DataType(DataType.DateTime,ErrorMessage ="Data em formato incorreto")]
+        [Required(ErrorMessage ="O campo data de lançamento é obrigatório")]
+        [Display(Name ="Data de Lançamento")]
+        public DateTime DataLancamento { get; set; }
+        
+        [RegularExpression(@"^[A-Z]+[a-z-A-Z\u00C0-\u00FF""'\w-]*$",ErrorMessage ="Genero em formato inválido")]
+        [StringLength(30,ErrorMessage ="Máximo de 30 caracteres")]
+        public string Genero { get; set; }
+        
+        [Range(1,1000,ErrorMessage ="Valor de 1 a 1000")]
+        [Required(ErrorMessage ="Preencha o campo valor")]
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal Valor { get; set; }
+
+        [RegularExpression(@"^[1-5]*$",ErrorMessage ="Somente números")]
+        [Required(ErrorMessage ="Preencha o campo avaliação")]
+        [Display(Name ="Avaliação")]
+        public int Avaliacao { get; set; }
+
+    }
+}
